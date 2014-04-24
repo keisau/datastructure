5cbe8f5ebf3c42fecee2dadc58579c5ce98ad45f
pierresaux <pierresaux@ymail.com>
2014-04-24 11:42:19 +0800

    Update radix_tree.h

--- radix_tree.h
+++ radix_tree.h
@@ -1,6 +1,7 @@
 #ifndef __DS_RADIX_TREE_H__
 #define __DS_RADIX_TREE_H__
 
+#include "class.h"
 #include "list.h"
 
 enum {
@@ -26,8 +27,7 @@ struct radix_data_node {
 
 #define rt_dnode(ptr) (container_of (ptr, struct radix_data_node, head))
 
-struct radix_tree * new_radix_tree ();
-void delete_radix_tree (struct radix_tree *tree);
+extern class radix_tree;
 void * radix_tree_insert (struct radix_tree *tree, unsigned long key, void *data);
 struct radix_data_node * radix_tree_find (struct radix_tree *tree, unsigned long key);
 void * radix_tree_delete (struct radix_tree *tree, unsigned long key);
