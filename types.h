2200e8e475b6286da66d7e2f39ae20af1f51ac0d
pierresaux <pierresaux@ymail.com>
2014-04-24 11:41:48 +0800

    Update types.h

--- types.h
+++ types.h
@@ -1,6 +1,21 @@
 #ifndef __DS_TYPES_H__
 #define __DS_TYPES_H__
 
+#include <stddef.h>
+#include <stdint.h>
+
+/**
+ * typedefs
+ */
+typedef int64_t				s64;
+typedef int32_t				s32;
+typedef int16_t				s16;
+typedef int8_t				s8;
+typedef uint64_t			u64;
+typedef uint32_t			u32;
+typedef uint16_t			u16;
+typedef uint8_t				u8;
+
 struct list_head {
 	struct list_head *next, *prev;
 };
@@ -12,4 +27,5 @@ struct hlist_head {
 struct hlist_node {
 	struct hlist_node *next, **pprev;
 };
+
 #endif // __DS_TYPES_H__
